5c47b3dbc65a12ad8874a7f734ae020e8dda48ea
abdullah107189 <abdullah107189@gmail.com>
2025-09-08 15:53:55 +0600

    set up axios on this project for better query handling

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+const config = {
+  baseApi: import.meta.env.VITE_BASE_API,
+};
+export default config;
---
+++ src/lib/axios.ts
@@ -0,0 +1,23 @@
+import config from "@/config";
+import axios from "axios";
+export const axiosInstance = axios.create({
+  baseURL: config.baseApi,
+});
+
+axios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function onFulfilled(response) {
+    return response;
+  },
+  function onRejected(error) {
+    return Promise.reject(error);
+  }
+);
